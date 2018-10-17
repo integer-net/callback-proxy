ae989af4edb95b4bc3601fb7b4ec84cf51552f81
Fabian Schmengler <fs@integer-net.de>
2018-10-17 10:07:23 +0200

    Add dev tools and configure

---
+++ tests/DispatcherTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace IntegerNet\CallbackProxy;
+
+use PHPUnit\Framework\TestCase;
+
+class DispatcherTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
